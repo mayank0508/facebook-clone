58d5ad7f233e51dc33bc74089f4360f55292781e
mayank0508 <thakurmayank0508@gmail.com>
2021-07-01 02:08:39 +0530

    tailwind css added 🍾

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,15 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>facebook</title> {/* here we inject the name of the bar */}
       </Head>
       <h1>Lets Build Facebook</h1>
-      <footer>
+      {/* <footer>
         <span>Made with ❤ by Mayank</span>
-      </footer>
+      </footer> */}
     </div>
   )
 }
