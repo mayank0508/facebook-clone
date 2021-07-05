0144f15b78bc29802fed9e64a5bbd135e912c091
mayank0508 <thakurmayank0508@gmail.com>
2021-07-06 02:38:30 +0530

    Header done 💤

--- components/Header.js
+++ components/Header.js
@@ -18,9 +18,12 @@ import HeaderIcon from './HeaderIcon';
 
 function Header() {
   return (
-    <div className="sticky top-0 z-50 bg-white flex 
-    item-center p-2 lg: px-5 shadow-md"> { /* here i have declared everthing for the header that how its looking, tbh form the icon to the end the main content */ }
-      
+    <div
+      className="sticky top-0 z-50 bg-white flex 
+    item-center p-2 lg: px-5 shadow-md"
+    >
+      {' '}
+      {/* here i have declared everthing for the header that how its looking, tbh form the icon to the end the main content */}
       {/* <h1>Header</h1>  {/* here we are declaring the header as you can see in diffrent diffrent divs !!!} */}
       {/* Left */}{' '}
       {/* her we will be having everthing in the rihgt side of the page */}
@@ -28,8 +31,8 @@ function Header() {
         <Image
           src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg"
           alt="Facebook"
-          width={40}
-          height={40}
+          width={55}
+          height={55}
           layout="fixed"
         />
         <div className="flex ml-2 items-centre rounded-full bg-gray-100 p-2">
@@ -38,7 +41,7 @@ function Header() {
           <SearchIcon className="h-6 color text-gray-600 m-2" />{' '}
           {/* here i have imported the searchIcon form the @heroIcon websit which i have imported further up the code lines */}
           <input
-            className="flex ml-2 items-center bg-transparent
+            className="hidden md:inline-flex ml-2 text-xl items-center bg-transparent
             outline-none placeholder-gray-500 flex-shrink"
             type="text"
             placeholder="Search Facebook"
@@ -59,6 +62,17 @@ function Header() {
         </div>
       </div>
       {/* Right */}
+      <div className="flex item-center sm:space-x-2 justify-end">
+        {/* profile picture will go here */}
+
+        <p className="whitespace-nonwrap font-semibold pr-3">
+          Mayank Kumar
+        </p>
+        <ViewGridIcon className="icon" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <ChevronDownIcon className="icon" />
+      </div>
     </div>
   );
 }
