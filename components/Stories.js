cfc33684a02073786eeec988532e2b8e6a6d9c91
mayank0508 <thakurmayank0508@gmail.com>
2021-07-09 09:50:45 +0530

    very very very major bug fixed by removing a s 😂😭

--- components/Stories.js
+++ components/Stories.js
@@ -4,29 +4,33 @@ const stories = [
   {
     name: 'Mayank Kumar',
     src: 'https://upload.wikimedia.org/wikipedia/commons/a/a8/Bill_Gates_2017_%28cropped%29.jp`g',
-    profiles:
+    profile:
       'https://upload.wikimedia.org/wikipedia/commons/a/af/Bill_Gates_-_Nov._8%2C_2019.jpg'
+  },
+  {
+    name: 'React',
+    src: 'https://upload.wikimedia.org/wikipedia/commons/a/a8/Bill_Gates_2017_%28cropped%29.jp`g',
+    profile:
+      'https://upload.wikimedia.org/wikipedia/commons/a/a8/Bill_Gates_2017_%28cropped%29.jpg'
+  },
+  {
+    name: 'Tailwind',
+    src: 'https://upload.wikimedia.org/wikipedia/commons/a/a8/Bill_Gates_2017_%28cropped%29.jpg',
+    profile:
+      'https://upload.wikimedia.org/wikipedia/commons/a/a8/Bill_Gates_2017_%28cropped%29.jpg'
+  },
+  {
+    name: 'Next.js',
+    src: 'https://upload.wikimedia.org/wikipedia/commons/a/a8/Bill_Gates_2017_%28cropped%29.jpg',
+    profile:
+      'https://upload.wikimedia.org/wikipedia/commons/a/a8/Bill_Gates_2017_%28cropped%29.jpg'
+  },
+  {
+    name: 'NextAuth',
+    src: 'https://upload.wikimedia.org/wikipedia/commons/a/a8/Bill_Gates_2017_%28cropped%29.jpg',
+    profile:
+      'https://upload.wikimedia.org/wikipedia/commons/a/a8/Bill_Gates_2017_%28cropped%29.jpg'
   }
-  //   {
-  //     name: 'React',
-  //     src: 'https://upload.wikimedia.org/wikipedia/commons/a/a8/Bill_Gates_2017_%28cropped%29.jp`g',
-  //     profiles: 'https://upload.wikimedia.org/wikipedia/commons/a/a8/Bill_Gates_2017_%28cropped%29.jpg'
-  //   },
-  //   {
-  //     name: 'Tailwind',
-  //     src: 'https://upload.wikimedia.org/wikipedia/commons/a/a8/Bill_Gates_2017_%28cropped%29.jpg',
-  //     profiles: 'https://upload.wikimedia.org/wikipedia/commons/a/a8/Bill_Gates_2017_%28cropped%29.jpg'
-  //   },
-  //   {
-  //     name: 'Next.js',
-  //     src: 'https://upload.wikimedia.org/wikipedia/commons/a/a8/Bill_Gates_2017_%28cropped%29.jpg',
-  //     profiles: 'https://upload.wikimedia.org/wikipedia/commons/a/a8/Bill_Gates_2017_%28cropped%29.jpg'
-  //   },
-  //   {
-  //     name: 'NextAuth',
-  //     src: 'https://upload.wikimedia.org/wikipedia/commons/a/a8/Bill_Gates_2017_%28cropped%29.jpg',
-  //     profiles: 'https://upload.wikimedia.org/wikipedia/commons/a/a8/Bill_Gates_2017_%28cropped%29.jpg'
-  //   }
 ];
 
 function Stories() {
