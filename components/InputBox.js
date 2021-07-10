8e3e0d585b85a1d566339994fb1de0c5de51ad18
mayank0508 <thakurmayank0508@gmail.com>
2021-07-11 03:34:20 +0530

    The upload clutton it ahppeningstarted

--- components/InputBox.js
+++ components/InputBox.js
@@ -2,30 +2,76 @@ import Image from 'next/image';
 import { useSession } from 'next-auth/client';
 import { EmojiHappyIcon } from '@heroicons/react/outline';
 import { CameraIcon, VideoCameraIcon } from '@heroicons/react/solid';
-import { db } from '../firebase';
-import { useRef } from 'react';
+import { db, storage } from '../firebase';
+import { useRef, useState } from 'react';
 import firebase from 'firebase';
 
 function InputBox() {
   const [session] = useSession();
   const inputRef = useRef(null);
+  const filepickerRef = useRef(null);
+  const [imageToPost, setImageToPost] = useState(null);
 
   const sendPost = e => {
     e.preventDefault();
 
     if (!inputRef.current.value) return;
 
-    db.collection('post').add({
-      message: inputRef.current.value,
-      name: session.user.name,
-      email: session.user.email,
-      image: session.user.image,
-      timestamp: firebase.firestore.FieldValue.serverTimestamp()
-    });
+    db.collection('post')
+      .add({
+        message: inputRef.current.value,
+        name: session.user.name,
+        email: session.user.email,
+        image: session.user.image,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp()
+      })
+      .then(doc => {
+        if (imageToPost) {
+          const uploadTask = storage
+            .ref(`post/${doc.id}`)
+            .putString(imageToPost, 'data_url');
+          removeImage();
+          uploadTask.on(
+            'state_change',
+            null,
+            error => console.error(error),
+            () => {
+              //when the upload completes
+              storage
+                .ref('posts')
+                .child(doc.id)
+                .getDownloadURL()
+                .then(url => {
+                  db.collection('posts').doc(doc.id).set(
+                    {
+                      postImage: url
+                    },
+                    { merge: true }
+                  );
+                });
+            }
+          );
+        }
+      });
 
     inputRef.current.value = '';
   };
 
+  const addImageToPost = e => {
+    const reader = new FileReader();
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
+
+    reader.onload = readerEvent => {
+      setImageToPost(readerEvent.target.result);
+    };
+  };
+
+  const removeImage = () => {
+    setImageToPost(null);
+  };
+ 
   return (
     <div className="bg-white p-2 rounded-2xl shadow-md text-gray-500 font-medium mt-6">
       <div className="flex space-x-4 p-4 item-center">
@@ -50,6 +96,16 @@ function InputBox() {
             Submit
           </button>
         </form>
+
+        {imageToPost && (
+          <div
+            onClick={removeImage}
+            className="flex flex-col filter hover:brightness-110 transition duration-150 transform hover:scale-105 cursor-pointer"
+          >
+            <img className="h-10 object-contain" src={imageToPost} alt="" />
+            <p className="text-xs text-red-500 text-cemter">Remove</p>
+          </div>
+        )}
       </div>
 
       <div className="flex justify-evenly p-3 border-t">
@@ -59,9 +115,19 @@ function InputBox() {
           <VideoCameraIcon className="h-7 text-red-500" />
           <p className="text-xs sm:text-sm xl:text-base">Live Video</p>
         </div>
-        <div className="inputIcon">
+        <div
+          onClick={() => filepickerRef.current.click()}
+          className="inputIcon"
+        >
           <CameraIcon className="h-7 text-green-500" />
           <p className="text-xs sm:text-sm xl:text-base">Photo/Video</p>
+          <input
+            ref={filepickerRef}
+            onChange={addImageToPost}
+            type="file"
+            hidden
+          />{' '}
+          {/* this is the way we add the small pic in the right side of the post */}
         </div>
         <div className="inputIcon">
           <EmojiHappyIcon className="h-7 text-yellow-500" />
